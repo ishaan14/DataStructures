a04b4087d038657954371410e45c5d3d6889de7a
Ishaan Garg <igarg142001@gmail.com>
2023-11-29 08:41:35 +0530

    intitial commit

---
+++ MaximumSubarraySum.java
@@ -0,0 +1,34 @@
+package slidingWindow;
+
+import java.util.ArrayList;
+
+public class Solution{
+    static long maximumSumSubarray(int K, ArrayList<Integer> Arr,int N){
+        // code here
+        int i=0;
+        int j=0;
+        long sum=0;
+        long max=Integer.MIN_VALUE;
+        
+        while(j<N)
+        {
+            sum+=Arr.get(j);
+            
+            if(j-i+1<K)
+            {
+                j++;
+            }
+            
+            else if(j-i+1==K)
+            {
+                max=Math.max(max,sum);
+                sum=sum-Arr.get(i);
+                i++;
+                j++;
+            }
+            
+        }
+        return max;
+        
+    }
+}
